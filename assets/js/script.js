ae8c356108526cafda1f04a06d640084f7f9da94
Daniel Lam <Dann.lam@gmail.com>
2023-04-06 19:00:34 -0700

    Event Listener 50% progress

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,14 +1,15 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-mainContainer = document.getElementById("main-container")
-
+let mainContainer = document.getElementById("main-container")
+let saveButton = document.querySelectorAll(".saveBtn")
 
 // TODO: Add code to display the current date in the header of the page.
 let heroTimer = setInterval(() => {
   //Select the currentHeaderDay text, in that text call on dayJS, which will return a crazy date, and format it like so. UPdate it with the setInterval every 1000ms.
+  //I set it to every minute because every second seemed excessive.
   $("#currentHeaderDay").text(dayjs().format("dddd MMMM,  DD"));
-}, 1000);
+}, 6000);
 
 
 //Maybe I could do something like...call dayjs, get the hh hour.
@@ -18,13 +19,14 @@ let heroTimer = setInterval(() => {
 
 $(init = () => {
 
-  console.log(dayjs().format("hh"))
+  console.log(mainContainer)
+
   for(let i = 9; i < 18; i++){
     //only iterates through the numbers 9 - 17, which is 9AM - 5PM.
 
     //gets elements by their hour ID.
     let element = document.getElementById("hour-" + i)
-    console.log(element);
+
 
 
     //compare it to the current hour and determine what their class should be.
@@ -38,11 +40,22 @@ $(init = () => {
       element.className = "row time-block past"
     }
   }
+  //Logic for eventListener
 
-//find the id that matches that hour
-//Go through the array of divs, we'll likely need to extract the hour from the hourID. Compare it to the current hour. If it's less then the current hour, alter the class to be "row time-block past."
-//If it's equal to the current hour, make the class "row time-block present".
-//If it's later to the current hour, make the class "row time-block future".
+
+  saveButton.addEventListener("click", function(event){
+    //When I click on a btn it should give me that button's parent's DIV ID which I will use to store as a key.
+    //The value inside the textarea will be saved as the value.
+    let currSelector = event.target
+
+    console.log(currSelector.parentNode);
+    let parentID = currSelector.parentNode.id;
+    let textAreaVal = currSelector.parentNode.children[1].value
+    console.log("---parentID---")
+    console.log(parentID)
+    console.log("---value---")
+    console.log(textAreaVal)
+  })
 
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
@@ -51,11 +64,6 @@ $(init = () => {
   // time-block containing the button that was clicked? How might the id be
   // useful when saving the description in local storage?
   //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
@@ -63,5 +71,3 @@ $(init = () => {
   //
 
 });
-
-init();
