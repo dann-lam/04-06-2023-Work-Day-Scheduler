06ab9d100b1285b677dd845b5c8255e40d3ae384
Daniel Lam <Dann.lam@gmail.com>
2023-04-06 19:37:24 -0700

    Fixed the event listener and added in the ability to set and get items

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,32 +1,30 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
-let mainContainer = document.getElementById("main-container")
-let saveButton = document.querySelectorAll(".saveBtn")
-
-// TODO: Add code to display the current date in the header of the page.
-let heroTimer = setInterval(() => {
-  //Select the currentHeaderDay text, in that text call on dayJS, which will return a crazy date, and format it like so. UPdate it with the setInterval every 1000ms.
-  //I set it to every minute because every second seemed excessive.
-  $("#currentHeaderDay").text(dayjs().format("dddd MMMM,  DD"));
-}, 6000);
-
-
-//Maybe I could do something like...call dayjs, get the hh hour.
-
-
 
 
 $(init = () => {
+  mainContainer = document.getElementById("main-container")
+  saveButton = document.getElementsByClassName("saveBtn")
 
-  console.log(mainContainer)
+  heroTimer = setInterval(() => {
+    //Select the currentHeaderDay text, in that text call on dayJS, which will return a crazy date, and format it like so. UPdate it with the setInterval every 1000ms.
+    //I set it to every minute because every second seemed excessive.
+    $("#currentHeaderDay").text(dayjs().format("dddd MMMM,  DD"));
+  }, 6000);
 
   for(let i = 9; i < 18; i++){
     //only iterates through the numbers 9 - 17, which is 9AM - 5PM.
+          //Logic to add in our previously saved values.
+    //Get our value by stringID
+    let currGetItem = localStorage.getItem("hour-" + i)
+
+    //Set that value into the currentID's value.
 
     //gets elements by their hour ID.
     let element = document.getElementById("hour-" + i)
-
+    //Sets the value of the text area to our saved value.
+    element.children[1].value = currGetItem
 
 
     //compare it to the current hour and determine what their class should be.
@@ -40,34 +38,23 @@ $(init = () => {
       element.className = "row time-block past"
     }
   }
-  //Logic for eventListener
 
 
-  saveButton.addEventListener("click", function(event){
+
+
+  //Logic for eventListener -- saves to local storage on click.
+    mainContainer.addEventListener("click", function(event){
     //When I click on a btn it should give me that button's parent's DIV ID which I will use to store as a key.
     //The value inside the textarea will be saved as the value.
-    let currSelector = event.target
+      let currSelector = event.target
+    if(event.target.tagName == "BUTTON"){
+        //Use the parentID as a key and its textAreaVal as a value and store it.
+      let parentID = currSelector.parentNode.id;
+      let textAreaVal = currSelector.parentNode.children[1].value
+      localStorage.setItem(parentID, textAreaVal);
+      }
 
-    console.log(currSelector.parentNode);
-    let parentID = currSelector.parentNode.id;
-    let textAreaVal = currSelector.parentNode.children[1].value
-    console.log("---parentID---")
-    console.log(parentID)
-    console.log("---value---")
-    console.log(textAreaVal)
   })
 
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
 
 });
