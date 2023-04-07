853b2fc669e6d3cc2dbb4d7162997ce25b505aed
Daniel Lam <Dann.lam@gmail.com>
2023-04-06 20:01:59 -0700

    Encased everything into a document ready

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,16 +2,18 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 
-
-$(init = () => {
+$(document).ready(function () {
+  setInterval(function () {
+    $("#currentHeaderDay").text(dayjs().format("dddd MMMM,  DD"));
+  }, 6000);
   mainContainer = document.getElementById("main-container")
   saveButton = document.getElementsByClassName("saveBtn")
 
-  heroTimer = setInterval(() => {
-    //Select the currentHeaderDay text, in that text call on dayJS, which will return a crazy date, and format it like so. UPdate it with the setInterval every 1000ms.
-    //I set it to every minute because every second seemed excessive.
-    $("#currentHeaderDay").text(dayjs().format("dddd MMMM,  DD"));
-  }, 6000);
+  // heroTimer = setInterval(() => {
+  //   //Select the currentHeaderDay text, in that text call on dayJS, which will return a crazy date, and format it like so. UPdate it with the setInterval every 1000ms.
+  //   //I set it to every minute because every second seemed excessive.
+  //   $("#currentHeaderDay").text(dayjs().format("dddd MMMM,  DD"));
+  // }, 6000);
 
   for(let i = 9; i < 18; i++){
     //only iterates through the numbers 9 - 17, which is 9AM - 5PM.
@@ -44,6 +46,7 @@ $(init = () => {
 
   //Logic for eventListener -- saves to local storage on click.
     mainContainer.addEventListener("click", function(event){
+      event.preventDefault();
     //When I click on a btn it should give me that button's parent's DIV ID which I will use to store as a key.
     //The value inside the textarea will be saved as the value.
       let currSelector = event.target
@@ -55,6 +58,57 @@ $(init = () => {
       }
 
   })
-
-
 });
+
+// mainContainer = document.getElementById("main-container")
+// saveButton = document.getElementsByClassName("saveBtn")
+
+// // heroTimer = setInterval(() => {
+// //   //Select the currentHeaderDay text, in that text call on dayJS, which will return a crazy date, and format it like so. UPdate it with the setInterval every 1000ms.
+// //   //I set it to every minute because every second seemed excessive.
+// //   $("#currentHeaderDay").text(dayjs().format("dddd MMMM,  DD"));
+// // }, 6000);
+
+// for(let i = 9; i < 18; i++){
+//   //only iterates through the numbers 9 - 17, which is 9AM - 5PM.
+//         //Logic to add in our previously saved values.
+//   //Get our value by stringID
+//   let currGetItem = localStorage.getItem("hour-" + i)
+
+//   //Set that value into the currentID's value.
+
+//   //gets elements by their hour ID.
+//   let element = document.getElementById("hour-" + i)
+//   //Sets the value of the text area to our saved value.
+//   element.children[1].value = currGetItem
+
+
+//   //compare it to the current hour and determine what their class should be.
+//   if (i == dayjs().format("H")){
+//     //If there is a match, then set its class to their appropriate coloring.
+//     element.className = "row time-block present";
+//     //parse the number because it's actually a string.
+//   } else if (i > parseInt(dayjs().format("H"))){
+//     element.className = "row time-block future";
+//   } else {
+//     element.className = "row time-block past"
+//   }
+// }
+
+
+
+
+// //Logic for eventListener -- saves to local storage on click.
+//   mainContainer.addEventListener("click", function(event){
+//     event.preventDefault();
+//   //When I click on a btn it should give me that button's parent's DIV ID which I will use to store as a key.
+//   //The value inside the textarea will be saved as the value.
+//     let currSelector = event.target
+//   if(event.target.tagName == "BUTTON"){
+//       //Use the parentID as a key and its textAreaVal as a value and store it.
+//     let parentID = currSelector.parentNode.id;
+//     let textAreaVal = currSelector.parentNode.children[1].value
+//     localStorage.setItem(parentID, textAreaVal);
+//     }
+
+// })
